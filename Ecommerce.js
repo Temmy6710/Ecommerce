7bcba0e0d49d119ffbb9a88812f2cc4f23439a52
temmy <132060906+Temmy6710@users.noreply.github.com>
2023-05-16 11:48:29 +0100

    automatic carousel

--- Ecommerce.js
+++ Ecommerce.js
@@ -270,26 +270,30 @@ function decrease(id){
 }
 
 let slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlide(n){
-    showSlides(slideIndex += n);
-}
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
+window.addEventListener("load",function() {
+    showSlides(slideIndex);
+    myTimer = setInterval(function(){plusSlides(1)}, 4000);
+})
 function showSlides(n){
-    console.log('hmmm')
-    let i;
-    let slide = document.getElementsByClassName('slide')
-    if (n > slide.length){
-        slideIndex = 1
+    var i;
+    let slides = document.getElementsByClassName('slide')
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
     }
-    if(n < 1){
-        slideIndex = slide.length
+    slides[slideIndex-1].style.display = "block";
+  }
+function plusSlides(n){
+    clearInterval(myTimer);
+    if (n < 0){
+      showSlides(slideIndex -= 1);
+    } else {
+     showSlides(slideIndex += 1); 
     }
-    for (i = 0; i < slide.length; i++) {
-        slide[i].style.display = "none";
+    if (n === -1){
+      myTimer = setInterval(function(){plusSlides(n + 2)}, 4000);
+    } else {
+      myTimer = setInterval(function(){plusSlides(n + 1)}, 4000);
     }
-    slide[slideIndex-1].style.display = "block";
 }
